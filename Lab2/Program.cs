f27d6ea1eb8ca370ed201431ca00e6430bc0e957
david <david.hess681@gmail.com>
2018-10-08 16:15:49 -0400

    Added comments

--- Lab2/Program.cs
+++ Lab2/Program.cs
@@ -10,6 +10,8 @@ namespace Lab2
     {
         static void Main(string[] args)
         {
+            // The whole process is put in a while loop. This way the user can continue using the program for as long as needed.
+
             bool cont = true;
 
             while (cont)
@@ -17,6 +19,8 @@ namespace Lab2
                 Console.WriteLine("Welcome to Grand Circus' Room Detail Generator!");
                 Console.WriteLine("");
                 Console.WriteLine("Enter Length:");
+
+                // Since user input is a string, we must parse it into a more usable data type.
 
                 double length = double.Parse(Console.ReadLine());
 
@@ -28,6 +32,8 @@ namespace Lab2
 
                 double height = double.Parse(Console.ReadLine());
 
+                // We declare the calculations as variables before displaying the answer to the user.
+
                 double perimeter = 2 * length + 2 * width;
                 double area = length * width;
                 double volume = area * height;
@@ -37,6 +43,9 @@ namespace Lab2
                 Console.WriteLine("Perimeter: " + perimeter);
                 Console.WriteLine("Volume: " + volume);
                 Console.WriteLine("");
+
+                // If at the end the user does not want to continue, cont is changed to false which closes the program.
+
                 Console.WriteLine("Continue? (y/n)");
 
                 if (Console.ReadLine() == "n")
@@ -44,9 +53,6 @@ namespace Lab2
                     cont = false;
                 }
                 Console.WriteLine("");
-
-                
-
             }
         }
         
